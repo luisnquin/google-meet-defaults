9d1cbd11278e50f6d3ebe8ef2f94966ae4aedd22
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-08-13 01:23:48 -0600

    draft: at least now it works

--- src/interceptor.js
+++ src/interceptor.js
@@ -2,24 +2,35 @@ const USE_MICROPHONE_KEY = 'USE_MICROPHONE',
     USE_CAMERA_KEY = 'USE_CAMERA',
     AUTH_USER_KEY = 'AUTH_USER'
 
+const AUTH_USER_SEARCH_PARAM_KEY = 'authuser'
+
 const transformUrl = (authUser) => {
-    const url = window.location.href
+    const url = new URL(window.location.href)
+    url.searchParams.set(AUTH_USER_SEARCH_PARAM_KEY, `${authUser}`)
 
-    const result = new URL(url)
-    result.searchParams.set('authuser', `${authUser}`)
-
-    return result.toString()
+    return url.toString()
 }
 
 const setStoredAuthUser = async () => {
-    const { key: authUser } = await chrome.storage.local.get([AUTH_USER_KEY])
+    const result = await chrome.storage.local.get([AUTH_USER_KEY])
+    if (!result) return
+
+    const authUser = result[AUTH_USER_KEY]
     if (!authUser) return
+
+    const params = new URLSearchParams(window.location.search)
+
+    if (params.get(AUTH_USER_SEARCH_PARAM_KEY) == authUser) {
+        return
+    }
 
     window.location.href = transformUrl(authUser)
 }
 
 setStoredAuthUser()
 
-chrome.storage.local.set([USE_MICROPHONE_KEY], true)
-chrome.storage.local.set([USE_CAMERA_KEY], true)
-chrome.storage.local.set([AUTH_USER_KEY], 1)
+const items = {}
+items[USE_MICROPHONE_KEY] = true
+items[USE_CAMERA_KEY] = true
+items[AUTH_USER_KEY] = 1
+chrome.storage.local.set(items)
